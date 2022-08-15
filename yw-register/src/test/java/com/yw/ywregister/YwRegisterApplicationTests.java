b3f84738e379e977206d3fcbe85774a1d1324227
yywzt <2507967396@qq.com>
2022-08-15 10:55:34 +0800

    feature: init gateway、register-center

---
+++ yw-register/src/test/java/com/yw/ywregister/YwRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yw.ywregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YwRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
