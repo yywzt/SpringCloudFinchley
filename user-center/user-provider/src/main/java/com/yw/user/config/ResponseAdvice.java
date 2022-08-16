37db52ba9712275bb0be5fb893e9365cc2d0aaba
yywzt <yywzt@foxmail.com>
2022-08-16 14:46:59 +0800

    feature: 全局异常处理器、全局接口返回统一格式

---
+++ user-center/user-provider/src/main/java/com/yw/user/config/ResponseAdvice.java
@@ -0,0 +1,41 @@
+package com.yw.user.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yyw.api.model.ResponseData;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @author yanzhitao@xiaomalixing.com
+ * @date 2022/8/16 14:30
+ */
+@RestControllerAdvice
+public class ResponseAdvice implements ResponseBodyAdvice<Object> {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @SneakyThrows
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType
+            , Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof String) {
+            return objectMapper.writeValueAsString(ResponseData.success(body));
+        }
+        if (body instanceof ResponseData) {
+            return body;
+        }
+        return ResponseData.success(body);
+    }
+}
