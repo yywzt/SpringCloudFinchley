678eea7d7247406e470e31a9d53535232d50ff33
严志涛 <yanzhitao@xiaomalixing.com>
2022-08-17 16:29:09 +0800

    feature: 控制器外全局异常处理

---
+++ task-center/task-provider/src/main/java/com/yw/task/config/DefaultErrorController.java
@@ -0,0 +1,41 @@
+package com.yw.task.config;
+
+import com.yyw.api.model.ResponseData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * @author yanzhitao@xiaomalixing.com
+ * @date 2021/5/14 17:16
+ */
+@Slf4j
+@RestController
+@RequestMapping("${server.error.path:${error.path:/error}}")
+public class DefaultErrorController extends AbstractErrorController {
+
+    @Autowired
+    public DefaultErrorController(ErrorAttributes errorAttributes) {
+        super(errorAttributes);
+    }
+
+
+    @RequestMapping
+    public ResponseEntity<ResponseData<Object>> doHandleError(HttpServletRequest request) {
+        Map<String, Object> body = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        HttpStatus status = getStatus(request);
+        log.error("status: {}, body: {}", status, body);
+        ResponseData<Object> result = ResponseData.failure(status.value() + "", body.get("error") + "," + body.get("message"));
+        return new ResponseEntity<>(result, status);
+    }
+
+}
